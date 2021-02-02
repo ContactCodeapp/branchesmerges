4ff8da24417e42886c4a78c8b3bfcc3e93a76b5c
Dominick Dougherty <ddougherty@contactcodeapp.com>
2021-02-01 20:54:34 -0500

    intail commit

---
+++ branches_mergers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace branches_mergers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
